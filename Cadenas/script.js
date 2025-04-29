16b1cf57ad6621f8c3975e962fe356599ad7dde0
Andrea Villegas <andyketzavg@gmail.com>
2025-04-29 00:05:15 -0600

    Tareas

---
+++ Cadenas/script.js
@@ -0,0 +1,8 @@
+
+let txt = "Tristeza";
+let txt2 = txt.toUpperCase();
+let txt3 = txt2.replace("CADENA", "STRINGS")
+document.getElementById('CADENA').innerHTML=txt;
+document.getElementById('CADENA2').innerHTML=txt2;
+document.getElementById('CADENA3').innerHTML=txt3;
+console.log(txt.toUpperCase());
---
+++ Tarea 3/script.js
@@ -0,0 +1,1 @@
+console.log("El arte de la comida");
